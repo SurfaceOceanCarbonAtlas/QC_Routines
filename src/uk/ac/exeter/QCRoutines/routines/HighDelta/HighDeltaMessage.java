1531cbbc37ec28770f3914ac38b1f673a8e2ea72
Steve Jones <steve@squaregoldfish.co.uk>
2016-03-31 19:31:46 +0100

    High Delta routine

---
+++ src/uk/ac/exeter/QCRoutines/routines/HighDelta/HighDeltaMessage.java
@@ -0,0 +1,23 @@
+package uk.ac.exeter.QCRoutines.routines.HighDelta;
+
+import uk.ac.exeter.QCRoutines.data.DataColumn;
+import uk.ac.exeter.QCRoutines.messages.Flag;
+import uk.ac.exeter.QCRoutines.messages.Message;
+
+public class HighDeltaMessage extends Message {
+
+	public HighDeltaMessage(int lineNumber, DataColumn dataColumn, double recordDelta, double maxDelta) {
+		super(lineNumber, dataColumn, Flag.BAD, String.valueOf(recordDelta), String.valueOf(maxDelta));
+	}
+
+	@Override
+	protected String getFullMessage() {
+		return columnName + " changed by " + fieldValue + " per minute - max is " + validValue + " per minute";
+	}
+
+	@Override
+	public String getShortMessage() {
+		return columnName + " changes too quickly";
+	}
+
+}
