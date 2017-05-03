376138318c2a12df36d4f50e0a24d684a69f8046
Steve Jones <steve@squaregoldfish.co.uk>
2017-05-03 11:57:39 +0200

    Javadoc

--- src/uk/ac/exeter/QCRoutines/data/InvalidDataException.java
+++ src/uk/ac/exeter/QCRoutines/data/InvalidDataException.java
@@ -2,10 +2,24 @@ package uk.ac.exeter.QCRoutines.data;
 
 import uk.ac.exeter.QCRoutines.config.ColumnConfigItem;
 
+/**
+ * An exception that is thrown when the data in a column is
+ * not of the type in its configuration.
+ * @author Steve Jones
+ *
+ */
 public class InvalidDataException extends DataRecordException {
 
+	/**
+	 * The Serial Version UID
+	 */
 	private static final long serialVersionUID = -3199169220532474396L;
 
+	/**
+	 * Main constructor
+	 * @param lineNumber The line number on which the error was found
+	 * @param sourceColumn The column whose data is of the incorrect type
+	 */
 	public InvalidDataException(int lineNumber, DataColumn sourceColumn) {
 		super(lineNumber, sourceColumn, "");
 	}
