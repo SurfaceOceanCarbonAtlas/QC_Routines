5566b71d91a8a795872cdf956ffe3c32aad1a6f8
Steve Jones <steve@squaregoldfish.co.uk>
2017-05-09 20:55:57 +0200

    Javadoc, and removed unused constructor

--- src/uk/ac/exeter/QCRoutines/messages/InvalidFlagException.java
+++ src/uk/ac/exeter/QCRoutines/messages/InvalidFlagException.java
@@ -1,14 +1,22 @@
 package uk.ac.exeter.QCRoutines.messages;
 
+/**
+ * Exception thrown when an invalid numeric flag value is encountered 
+ * @author Steve Jones
+ *
+ */
 public class InvalidFlagException extends Exception {
 
+	/**
+	 * The Serial Version UID
+	 */
 	private static final long serialVersionUID = 1787207060024257945L;
 
+	/**
+	 * Constructor
+	 * @param flagValue The invalid flag value 
+	 */
 	public InvalidFlagException(int flagValue) {
 		super("Invalid flag value " + flagValue);
-	}
-	
-	public InvalidFlagException(String message) {
-		super(message);
 	}
 }
