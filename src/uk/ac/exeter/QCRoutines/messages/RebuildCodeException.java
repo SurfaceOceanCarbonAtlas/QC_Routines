a71c08ebece50ce1e5028de9d44fe3cb566bf6fc
Steve Jones <steve@squaregoldfish.co.uk>
2017-07-18 16:28:21 +0200

    Javadoc

--- src/uk/ac/exeter/QCRoutines/messages/RebuildCodeException.java
+++ src/uk/ac/exeter/QCRoutines/messages/RebuildCodeException.java
@@ -1,9 +1,21 @@
 package uk.ac.exeter.QCRoutines.messages;
 
+/**
+ * Exception thrown from invalid rebuild code strings
+ * @author Steve Jones
+ * @see RebuildCode
+ */
 public class RebuildCodeException extends MessageException {
 
+	/**
+	 * The serial version UID
+	 */
 	private static final long serialVersionUID = -8843114125586814644L;
 
+	/**
+	 * Basic constructor
+	 * @param message The error message
+	 */
 	public RebuildCodeException(String message) {
 		super("Invalid message rebuild code: " + message);
 	}
