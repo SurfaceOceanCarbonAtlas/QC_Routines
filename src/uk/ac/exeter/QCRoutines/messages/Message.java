8b4df3c60ba5c0440716f4042176e8624258960a
Steve Jones <steve@squaregoldfish.co.uk>
2018-01-08 15:54:38 +0100

    Line numbers are longs

--- src/uk/ac/exeter/QCRoutines/messages/Message.java
+++ src/uk/ac/exeter/QCRoutines/messages/Message.java
@@ -324,7 +324,7 @@ public abstract class Message {
 		boolean hasConstructor = true;
 		
 		try {
-			Constructor<?> constructor = messageClass.getConstructor(int.class, TreeSet.class, TreeSet.class, Flag.class, String.class, String.class);
+			Constructor<?> constructor = messageClass.getConstructor(long.class, TreeSet.class, TreeSet.class, Flag.class, String.class, String.class);
 			
 			// Check that the Set is for Integers
 			Type[] constructorGenericTypes = constructor.getGenericParameterTypes();
